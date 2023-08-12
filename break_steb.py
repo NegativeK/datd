4484c5d652858a918f8058a8772b5c7f45afde79
negk <negk@negk.net>
2023-08-12 10:02:19 -0700

    Try all caesar cipher possibilities

--- break_steb.py
+++ break_steb.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import itertools
+import secretpy
 
 steb_letters = [
     "nprtx",
@@ -39,14 +40,25 @@ def check_stebs(possibs, english_words):
     return all_chances
 
 
+def caesar_them(possibs, shift_amt):
+    cipher = secretpy.Caesar()
+    pts = []
+    for pos in possibs:
+        pt = cipher.decrypt(pos, shift_amt, secretpy.alphabets.ENGLISH)
+        pts.append(pt)
+
+    return pts
+
+
 def main():
     possibs = permute_steb(steb_letters)
     possibs = ["".join(x) for x in possibs]
     english_words = get_english()
-    # eight_words = [x for x in english_words if len(x) == 8]
-    matching_stebs = check_stebs(possibs, english_words)
-    print(matching_stebs)
-    breakpoint()
+    eight_words = [x for x in english_words if len(x) == 8]
+    for i in range(26):
+        pts = caesar_them(possibs, i)
+        pts = check_stebs(pts, eight_words)
+        print("\n".join(pts))
 
 
 main()
